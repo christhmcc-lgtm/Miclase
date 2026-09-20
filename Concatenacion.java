93be9cd8d67adfaaac020dab4fd37e35f97d29db
christhmcc-lgtm <christhmcc@gmail.com>
2026-09-20 17:56:49 -0500

    Add files via upload

---
+++ Concatenacion.java
@@ -0,0 +1,10 @@
+class Concatenacion {
+public static void main(String[] args) {
+String nombre = "Christopher";
+String primerApellido = "Monago";
+String nombreConApellido =
+nombre + " " + primerApellido;
+
+System.out.println(nombreConApellido);
+} // Fin del método main
+} // Fin de la clase Concatenacion
---
+++ SecuenciasDeEscape.java
@@ -0,0 +1,11 @@
+class SecuenciasDeEscape {
+public static void main(String[] args) {
+System.out.println("Me dejo con el amor en las manos\nY si ya no me quiere");
+System.out.println("Solo me uso\tYa no quiere");
+System.out.println("No le importe\rElla no me quiere");
+System.out.println(
+"\"Se beso a otro\" Y asi acabo");
+System.out.println(
+"Mala\\ Mujer");
+} // Fin del método main
+} // Fin de la clase SecuenciasDeEscape
